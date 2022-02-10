38609e2df6cbabb7fae361e8b2ace5928ef6db1e
Laqi17 <89798279+Laqi17@users.noreply.github.com>
2022-02-10 19:49:25 +0200

    Update 4.sql
    refactor

--- 4.sql
+++ 4.sql
@@ -2,17 +2,37 @@ ALTER TABLE projects
 ADD cost INT;
 
 UPDATE projects
-SET cost = 10000
+SET cost = (
+SELECT sum(d.salary) FROM developers d
+join developers_projects dp on d.developer_id = dp.developer_id
+join projects pr on pr.project_id = dp.project_id
+WHERE pr.project_id = 1
+)
 WHERE project_id = 1;
 
 UPDATE projects
-SET cost = 20000
+SET cost = (
+SELECT sum(d.salary) FROM developers d
+join developers_projects dp on d.developer_id = dp.developer_id
+join projects pr on pr.project_id = dp.project_id
+WHERE pr.project_id = 2
+)
 WHERE project_id = 2;
 
 UPDATE projects
-SET cost = 5000
+SET cost = (
+SELECT sum(d.salary) FROM developers d
+join developers_projects dp on d.developer_id = dp.developer_id
+join projects pr on pr.project_id = dp.project_id
+WHERE pr.project_id = 3
+)
 WHERE project_id = 3;
 
 UPDATE projects
-SET cost = 7000
+SET cost = (
+SELECT sum(d.salary) FROM developers d
+join developers_projects dp on d.developer_id = dp.developer_id
+join projects pr on pr.project_id = dp.project_id
+WHERE pr.project_id = 4
+)
 WHERE project_id = 4;
